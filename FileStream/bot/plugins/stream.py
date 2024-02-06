b83f3391161be4802bd6eae4278b9045f6feb104
Shadow <145957007+misterrnova@users.noreply.github.com>
2024-02-07 03:04:42 +0530

    Update stream.py

--- FileStream/bot/plugins/stream.py
+++ FileStream/bot/plugins/stream.py
@@ -13,7 +13,7 @@ db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 
 @FileStream.on_message(filters.private& (filters.document| filters.video),group=4,)
 async def private_receive_handler(bot: Client, message: Message):
-    up=await message.reply_text("generating streaming link")
+    up=await message.reply_text("generating streaming link ⚡")
     
     if Telegram.FORCE_SUB:
         if not await is_user_joined(bot, message):
@@ -38,8 +38,8 @@ async def private_receive_handler(bot: Client, message: Message):
     finally:
         await up.delete()
         await is_user_exist(bot, message)
-        await pst.delete()
         await asyncio.sleep(600)
+        await pst.delete()
 
 
 # @FileStream.on_message(
