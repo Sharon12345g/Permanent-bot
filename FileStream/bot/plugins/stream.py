01fce5f00854bea4bdc40b480cc623030c333e88
Shadow <145957007+misterrnova@users.noreply.github.com>
2024-02-07 03:56:26 +0530

    Update stream.py

--- FileStream/bot/plugins/stream.py
+++ FileStream/bot/plugins/stream.py
@@ -40,6 +40,7 @@ async def private_receive_handler(bot: Client, message: Message):
 
     finally:
         await up.delete()
+        await message.delete()
         await is_user_exist(bot, message)
         await asyncio.sleep(600)
         await pst.delete()
