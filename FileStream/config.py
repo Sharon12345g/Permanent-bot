81a42f0fc147a53e676a598ebffb91e55327ff1e
Shadow <145957007+misterrnova@users.noreply.github.com>
2024-02-07 00:59:13 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -17,8 +17,8 @@ class Telegram:
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
     FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
-    START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
-    VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
+    START_PIC = env.get('START_PIC', "https://telegra.ph/file/2336034b8c89c6bba8958.jpg")
+    VERIFY_PIC = env.get('VERIFY_PIC', "https://telegra.ph/file/2336034b8c89c6bba8958.jpg")
     MULTI_CLIENT = False
     FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002131280557"))   # Logs channel for file logs
     ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002131280557"))   # Logs channel for user logs
