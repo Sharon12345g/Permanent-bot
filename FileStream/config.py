e6894178c9fcf65bc5c847c00fd0b2976b83175f
Shadow <145957007+misterrnova@users.noreply.github.com>
2024-02-07 21:23:49 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -35,4 +35,4 @@ class Server:
     URL = "http{}://{}{}/".format(
         "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ""
     )
-    KEEP_ALIVE = str(env.get("KEEP_ALIVE", "1").lower()) in ("1", "true", "t", "yes", "y")
+    KEEP_ALIVE = str(env.get("KEEP_ALIVE", "0").lower()) in ("1", "true", "t", "yes", "y")
