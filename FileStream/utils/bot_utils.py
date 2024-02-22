58643cce41c303eede19eb59c1979bb40842d074
Shadow <145957007+shadow603@users.noreply.github.com>
2024-02-22 18:06:15 +0530

    Update bot_utils.py

--- FileStream/utils/bot_utils.py
+++ FileStream/utils/bot_utils.py
@@ -99,7 +99,7 @@ async def gen_link(_id):
         stream_text = LANG.STREAM_TEXT.format(file_name, file_size, stream_link, page_link)
         reply_markup = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ 📂", url=stream_link),InlineKeyboardButton("sᴛʀᴇᴀᴍ ⚡", url=page_link) ]
+                [InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ  📂", url=stream_link),InlineKeyboardButton("sᴛʀᴇᴀᴍ  ⚡", url=page_link) ]
             ]
         )
     else:
