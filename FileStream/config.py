51bf5c5460f2455cf86cc14b5e92a76acf0bf990
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2024-03-08 19:52:58 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -2,24 +2,24 @@ from os import environ as env
 from dotenv import load_dotenv
 load_dotenv()
 class Telegram:
-    API_ID = int(env.get("API_ID","3704207"))
-    API_HASH = str(env.get("API_HASH","8d20e46f5413139329f2ec753f7c482a"))
-    BOT_TOKEN = str(env.get("BOT_TOKEN","6739797423:AAEPGYx-zof8vBl-J3bXQ0bFhuVKW2u8Ecw"))
-    OWNER_ID = int(env.get('OWNER_ID', '1531899507'))
+    API_ID = int(env.get("API_ID","1736204"))
+    API_HASH = str(env.get("API_HASH","890d40e0f91a4de32dec2965444b2cbe"))
+    BOT_TOKEN = str(env.get("BOT_TOKEN","6649780057:AAGbjaDAjm0A2m1qR7ZD-bJujhAaj0S5dcA"))
+    OWNER_ID = int(env.get('OWNER_ID', '1058015838'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL',"mongodb+srv://vegafiletolinkbot:vegafiletolinkbot@cluster0.knjtwz3.mongodb.net/?retryWrites=true&w=majority"))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "VegaLatest"))
+    DATABASE_URL = str(env.get('DATABASE_URL',"mongodb+srv://filetolink:filetolink@filetolink.vaepsfk.mongodb.net/?retryWrites=true&w=majority&appName=filetolink"))
+    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "heroflix"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
-    FORCE_SUB_ID = env.get('FORCE_SUB_ID', "-1002086574998")
+    FORCE_SUB_ID = env.get('FORCE_SUB_ID', "-1001521700370")
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', True)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
-    FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
-    START_PIC = env.get('START_PIC', "https://telegra.ph/file/2336034b8c89c6bba8958.jpg")
-    VERIFY_PIC = env.get('VERIFY_PIC', "https://telegra.ph/file/2336034b8c89c6bba8958.jpg")
+    FILE_PIC = env.get('FILE_PIC', https://te.legra.ph/file/b110eb2c5d4bd01b4537c.jpg")
+    START_PIC = env.get('START_PIC', "https://te.legra.ph/file/b110eb2c5d4bd01b4537c.jpg")
+    VERIFY_PIC = env.get('VERIFY_PIC', "https://te.legra.ph/file/b110eb2c5d4bd01b4537c.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002089395206"))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002089395206"))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1001652564383"))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1001652564383"))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
