359798f525f6b45defb3eb4e0baa26600317d0e9
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2024-03-09 08:58:56 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -25,7 +25,7 @@ class Telegram:
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
 class Server:
     PORT = int(env.get("PORT", 8080))
-    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
+    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "https://git.heroku.com/fileto.git"))
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "1").lower()) in ("1", "true", "t", "yes", "y")
